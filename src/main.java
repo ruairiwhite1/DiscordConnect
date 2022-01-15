e526cf756e68e2c229f05920de3098abd20f07ea
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2022-01-15 20:38:10 +0000

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,27 @@
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin implements Listener {
+    public void onEnable() {
+        System.out.println("------------------------");
+        System.out.println("                        ");
+        System.out.println("     DiscordConnect ");
+        System.out.println(" Developed by Ruairiw8!");
+        System.out.println(" Verification made easy");
+        System.out.println("     Now loading...");
+        System.out.println("                        ");
+        System.out.println("------------------------");
+    }
+
+    public void onDisabe() {
+        System.out.println("------------------------");
+        System.out.println("                        ");
+        System.out.println("     DiscordConnect ");
+        System.out.println(" Developed by Ruairiw8!");
+        System.out.println(" Verification made easy");
+        System.out.println("     Now disabling..");
+        System.out.println("                        ");
+        System.out.println("------------------------");
+    }
+
+}
